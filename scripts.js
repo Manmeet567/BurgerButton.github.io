a48b58836464a1e8faefca80de4f8e24c71e3523
Manmeet567 <74705756+Manmeet567@users.noreply.github.com>
2022-03-19 18:16:07 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded',function(){
-    document.querySelector('.menu-toggler').onclick = function(){
-        document.querySelector('.menu-toggler').classList.toggle('active');
-        document.querySelector('.navbar-menu').classList.toggle('active');
-    }
-})
